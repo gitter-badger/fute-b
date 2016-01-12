5bd0747af5f8272d29e1111470d77dae6c886ad2
Márcio Pinheiro <mpinheiro@ciandt.com>
2016-01-12 20:10:55 -0200

    First commit scaffolding by yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name futeBApp
+ * @description
+ * # futeBApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('futeBApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name futeBApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the futeBApp
+ */
+angular.module('futeBApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
